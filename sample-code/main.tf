0ac9a7fb942e751cf57e6fef6a0538c828c5d2ae
RajanikanthGitHub <rajanikanth86@gmail.com>
2021-06-03 23:16:56 +0100

    .gitignore file added and location variable updated

--- sample-code/main.tf
+++ sample-code/main.tf
@@ -2,7 +2,7 @@
 # Create a resource group
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
-  location = var.location
+  location = "${var.location}"
 }
 
 # Create a virtual network within the resource group
